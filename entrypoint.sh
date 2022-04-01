9f55a751fe77ffeb38cf0000689c4cfba1f4eb60
Sha <sha@detalytics.com>
2022-04-01 14:17:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ export SETUPTOOLS_USE_DISTUTILS=stdlib
 
 echo "[action-pipenv-setup] pipenv-setup ${INPUT_ARGS}"
 exit_val="0"
-output = "$(pipenv-setup ${INPUT_ARGS} 2>&1)" || exit_val="$?"
+output="$(pipenv-setup ${INPUT_ARGS} 2>&1)" || exit_val="$?"
 
 echo "::set-output name=output::$output"
 
 # Throw error if an error occurred and fail_on_error is true
-if [[ "${INPUT_FAIL_ON_ERROR,,}" = 'true' && "${exit_val}" -eq "1" ]]; then
+if [[ "${INPUT_FAIL_ON_ERROR}" = 'true' && "${exit_val}" -eq "1" ]]; then
   exit 1
 fi
