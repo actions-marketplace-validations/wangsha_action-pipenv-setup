5d66325585113a61169f059229770475d9f1a24e
Sha <sha@detalytics.com>
2022-04-01 14:28:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 export SETUPTOOLS_USE_DISTUTILS=stdlib
-
+echo "fail on error: ${INPUT_FAIL_ON_ERROR}"
 echo "[action-pipenv-setup] pipenv-setup ${INPUT_ARGS}"
 exit_val="0"
 output="$(pipenv-setup ${INPUT_ARGS} 2>&1)" || exit_val="$?"
